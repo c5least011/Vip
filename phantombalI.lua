1d75d2dc3be5759a17ff83c367a6049685b8998f
c5least011 <tuan0976156938@gmail.com>
2025-07-13 04:34:36 +0700

    Add files via upload

---
+++ phantombalI.lua
@@ -0,0 +1,194 @@
+ÿþolac llPyare s =agemG:teeSvrci(eP"aleysr)"    l
+colaT ewneeSvrci e =agemG:teeSvrci(eT"ewneeSvrci"e )  
+ olac lIV M =agemG:teeSvrci(eV"riutlanIupMtnagare)"    l
+colal  p =lPyare.soLacPlaley r  
+     l
+colaR yaifle d =oldatsirgng(ma:etHpteG(th"ttsp/:s/riui.semunr/yaifle"d)))(     
+  
+ olac liWdnwo= R yaifle:drCaeetiWdnwo{(    	
+aNem= " hPnaot maBll,"    	
+oLdaniTgtiel= " kSbidi"i ,  
+ L	aoidgnuStbtiel= " kSbidi"i ,  
+ C	noifugaritnoaSivgn= { nEbael d =afsl}e ,  
+ D	siocdr= { nEbael d =afsl}e ,  
+ K	yeyStsme= f laes    }
+ )  
+     l
+colaM iaTnba= W niod:wrCaeetaT(bM"ia"n ,4438634285 )  
+ olac leKTyba= W niod:wrCaeetaT(bK"yeibdn"s ,4438634285 )  
+     l
+colaf ylni,gs apEm ,ptaBll= f laes ,afsl,ef laes    l
+colal saPtrayr= 0     l
+colao iriganClloro= n li    l
+colac ahgnde= f laes    l
+colaa gnel= 0     l
+colap ersseK y =nEmuK.yeoCedF.     
+  
+ aMniaT:brCaeetoTggel{(    	
+aNem= " oFllwoB la"l ,  
+ C	ruertnaVul e =afsl,e    	
+aCllabkc= f nutcoi(n)vf ylni g = vne d  
+ )}     
+  
+ aMniaT:brCaeetoTggel{(    	
+aNem= " pSmaB olkc,"    	
+uCrrneVtlaeu= f laes ,  
+ C	lablca k =ufcnitnov( )psma E = vne d  
+ )}     
+  
+ aMniaT:brCaeetoTggel{(    	
+aNem= " PTB la"l ,  
+ C	ruertnaVul e =afsl,e    	
+aCllabkc= f nutcoi(n)vt Bpla l = vne d  
+ )}     
+  
+ aMniaT:brCaeetuBttno{(    	
+aNem= " wTee notP al yrAae,"    	
+aCllabkc= f nutcoi(n )  
+ 		olac lhcra= l .phCracaet r  
+ 		fin toc ah rron toc ah:riFdniFsrCtihdl"(uHamondioRtoaPtr)"t eh nerutnre dn    	
+l	colap so= V ceot3rn.we-(82.3303596780364,92 203.288325350926,5- 339.871327064712)9    	
+l	colah pr= c ah.ruHamondioRtoaPtr    	
+l	colad si t =h(prP.sotioi n -op)sM.gainuted    	
+T	ewneeSvrci:erCaeeth(pr ,wTeeInfn.oen(widts/ 1 00 ,nEmuE.saniSgytelL.niae)r , {oPisitno= p so} :)lPya)(    	
+ne d  
+ )}     
+  
+ aMniaT:brCaeetuBttno{(    	
+aNem= " oFcr eeRes"t ,  
+ C	lablca k =ufcnitno)(    	
+l	colac ah r =plC.ahartcre    	
+i	 fhcrat eh n  
+ 		l	colah mu= c ah:riFdniFsrCtihdlfOlCsa(sH"munaio"d )  
+ 		i	 fuh mhtneh muH.aetl h = 0ne d  
+ 		ne d  
+ e	dn    }
+ )  
+     -
+ -UG IlCci keK y  
+ olac lcSerneuG i =nItsnaecn.we"(cSerneuG"i ,agemG:teeSvrci(eC"roGeiu)" )  
+ cSerneuG.iaNem= " oFcrEeU_"I    S
+rceeGniuE.anlbde= f laes     
+  
+ olac lubttno= I snatcn.een(wT"xeBttuot"n ,cSerneuG)i    b
+tuot.niSez= U iD2mn.we0( ,01,00  ,04 )  
+ ubttnoP.sotioi n =DUmi.2en(w.0,5- 05 ,.0,90  )  
+ ubttnoB.cagkornuCdloro 3 =oCol3rf.orRmBG2(55 ,08 ,08 )  
+ ubttnoT.xeCtloro 3 =oCol3rn.we1( ,,11  )  
+ ubttnoF.no t =nEmuF.no.toGhtmaoBdl    b
+tuot.neTtxiSez= 1  4  
+ ubttnoT.xe t =B"olkcU "i     
+  
+ ubttnoM.uoesuBttnoC1ilkcC:noentcf(nutcoi(n )  
+ l	saPtrayr= t ci(k )  
+ c	ahgnde= f laes    	
+IV:MeSdneKEyevtnt(ur,ep ersseK,yf laes ,agem )  
+ t	sa.kawti0(0.)5    	
+IV:MeSdneKEyevtnf(laes ,rpseKsye ,afsl,eg ma)e    	
+agemS.attrreuG:ieSCtro(eS"neNdtoficitaoi"n , {  
+ 		iTlt e =S"rcpi"t ,  
+ 		eTtx= t sortni(grpseKsye.)".c ilkcde,"    	
+D	rutaoi n = 1  
+ }	 )  
+ ne)d     
+  
+ aMniaT:brCaeetoTggel{(    	
+aNem= " hSwoB olkcU "i ,  
+ C	ruertnaVul e =afsl,e    	
+aCllabkc= f nutcoi(n)vS rceeGniuE.anlbde= v e dn    }
+ )  
+     -
+ -pSmaK ye    t
+sa.kpswa(nufcnitno)(    	
+hwli eatksw.ia(t.000)1d  o  
+ 		fis apEmt eh n  
+ 		V	MIS:neKdyevEne(trteu ,rpseKsye ,afsl,eg ma)e    	
+		atksw.ia(t.050 )  
+ 		V	MIS:neKdyevEne(tafsl,ep ersseK,yf laes ,agem )  
+ 		ne d  
+ e	dn    e
+dn )  
+     -
+ -oFllwoB la llF y  
+ atkss.apnwf(nutcoi(n )
+ w	ihelt sa.kawti)(d  o
+ 		fin tof ylni ghtnec noitun ene d
+ 		fit ci(k ) -altsaPrr y <.030t eh noctnnieue dn  	
+l	colac ah r =plC.ahartcre  	
+i	 fon thcrao  ron thcraF:niFdritshCli(dH"munaioRdooPtra"t )htnec noitun ene d
+ 		olac lrh p =hcraH.munaioRdooPtra t
+ 		olac labll= w roskapecF:niFdritshCli(dG"maBela"l )
+ 		fin tob la lhtnec noitun ene d
+ 		olac locol r =abllC.loro  	
+i	 fon troginilaoCol rhtneo iriganClloro= c loroe dn  	
+i	 focol r=~o iriganClloroa dnn toc ahgndet eh n
+ 		c	ahgnde= t ur e
+ 		l	saPtrayr= t ci(k )
+ 		g	ma.etSraetGriuS:teoCer"(eSdnoNitifacitno,"{ 	
+		T	tiel= " aBllD tece"t
+,				eTtx= " lF y +lCci"k
+,				uDaritno= 1 	
+		)}  	
+		IV:MeSdneKEyevtnt(ur,ep ersseK,yf laes ,agem )
+ 		t	sa.kawti0(0.)5  	
+		IV:MeSdneKEyevtnf(laes ,rpseKsye ,afsl,eg ma)e  	
+e	dn  	
+i	 focol r==o iriganClloroa dnc ahgndet eh n
+ 		c	ahgnde= f laes  	
+e	dn  	
+l	colar daui s =hcnaeg dna d02o  r05  	
+a	gnel+  =amhtr.da1()0  	
+l	colax = m ta.hoc(snalg)e* r daui s
+ 		olac l z =amhts.nia(gnel ) *aridsu  	
+l	colao ffes t =eVtcro.3en(w,x0  ,)z  	
+l	colap eridtcdeoP s =abllP.sotioi n +abllA.ssmelbLyniaeVrlecoti y *.0 2
+ 		olac latgrteoP s =rpdecietPdso+ o ffes t
+ 		wTeeSnreivecC:erta(erh,pT ewnenIofn.we0(0.,1E un.maEisgntSly.eiLenra,){ 	
+		oPisitno= t raegPtso	
+}	:)lPya)(  	
+ne d
+ ne)d   
+
+ --T  PaBllD tece t
+ atkss.apnwf(nutcoi(n )
+ w	ihelt sa.kawti)(d  o
+ 		fin tot Bpla lhtnec noitun ene d
+ 		fit ci(k ) -altsaPrr y <.030t eh noctnnieue dn  	
+l	colac ah r =plC.ahartcre  	
+i	 fon thcrao  ron thcraF:niFdritshCli(dH"munaioRdooPtra"t )htnec noitun ene d
+ 		olac lrh p =hcraH.munaioRdooPtra t
+ 		olac labll= w roskapecF:niFdritshCli(dG"maBela"l )
+ 		fin tob la lhtnec noitun ene d
+ 		olac locol r =abllC.loro  	
+i	 fon troginilaoCol rhtneo iriganClloro= c loroe dn  	
+i	 focol r=~o iriganCllorot eh n
+ 		l	saPtrayr= t ci(k )
+ 		o	iriganClloro= c loro  	
+		agemS.attrreuG:ieSCtro(eS"neNdtoficitaoi"n ,
+{				iTlt e =T" PaBll,"	
+		T	xe t =B"la lhcnaeg!dT  P +lCci!k,"	
+		D	rutaoi n =
+1		}	 )
+ 		V	MIS:neKdyevEne(trteu ,rpseKsye ,afsl,eg ma)e  	
+		IV:MeSdneKEyevtnf(laes ,rpseKsye ,afsl,eg ma)e  	
+		olac lid r =h(prP.sotioi n -abllP.sotioi)nU.in t
+ 		l	colap so= b la.loPisitno+ d ri* 4   	
+		rh.pFCarem= C rFma.een(wop)s  	
+e	dn  	
+ne d
+ ne)d   
+
+ --K yeibdn s
+ eKTybaC:ertaBetuot(n {  
+ N	ma e =C"ilkcF ,"    	
+aCllabkc= f nutcoi(n )  
+ 		rpseKsye= E un.meKCydo.e F  
+ e	dn    }
+ )  
+     K
+yeaT:brCaeetuBttno{(    	
+aNem= " lCci k"E ,  
+ C	lablca k =ufcnitno)(    	
+p	ersseK y =nEmuK.yeoCedE.    	
+ne d  
+ )}
